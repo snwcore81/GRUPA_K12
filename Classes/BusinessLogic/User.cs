af7d8d61c8f4a6f86e94ff99f9f0c1f774e23866
Jacek Kuźmicz <snwcore@gmail.com>
2021-03-28 09:33:02 +0200

    pierwsza iteracja - mechanizm serializacji/deserializacji

---
+++ Classes/BusinessLogic/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace GRUPA_K12.Classes.BusinessLogic
+{
+    [DataContract]
+    public class User : XmlStorage<User> 
+    {
+        [DataMember]
+        public string Login { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+
+        public User()
+        {
+            this.BaseObject = this;
+        }
+        
+        public override bool InitializeFromObject(User Object)
+        {
+            this.Login = Object.Login;
+            this.Password = Object.Password;
+
+            return true;
+        }
+    }
+}
---
+++ Classes/XmlStorage.cs
@@ -0,0 +1,58 @@
+﻿using GRUPA_K12.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
+
+namespace GRUPA_K12.Classes
+{
+    public static class XmlStorageTypes
+    {
+        private static readonly List<Type> KnownTypes = new List<Type>() { typeof(object) };
+
+        public static void Register<T>()
+        {
+            Type _oType = typeof(T);
+
+            if (!KnownTypes.Contains(_oType))
+            {
+                KnownTypes.Add(_oType);
+            }
+        }
+
+        public static Type[] GetArray() => KnownTypes.ToArray();
+    }
+
+    [DataContract]
+    public abstract class XmlStorage<T> : IXmlStorage where T : class
+    {
+        [IgnoreDataMember]
+        public T BaseObject { get; protected set; }
+        
+        public virtual bool FromXml(Stream a_oStream)
+        {
+            DataContractSerializer _oSerializer = new DataContractSerializer(typeof(T), XmlStorageTypes.GetArray());
+
+            using var _oReader = XmlDictionaryReader.CreateTextReader(a_oStream, new XmlDictionaryReaderQuotas());            
+
+            return InitializeFromObject((T)_oSerializer.ReadObject(_oReader, false));
+        }
+
+        public virtual MemoryStream ToXml()
+        {
+            DataContractSerializer _oSerializer = new DataContractSerializer(typeof(T), XmlStorageTypes.GetArray());
+
+            using var _oStream = new MemoryStream();
+
+            using var _oWriter = XmlDictionaryWriter.CreateTextWriter(_oStream, Encoding.UTF8);
+
+            _oSerializer.WriteObject(_oWriter, this);
+
+            return _oStream;            
+        }
+
+        public abstract bool InitializeFromObject(T Object);
+    }
+}
